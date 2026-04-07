904dfd500cf8041bcb4bdfc1271087e9f17f5acc
Ernesto <erodriiguezz@icloud.com>
2026-04-07 16:41:49 -0400

    Create 04-07-palindrome-characters.js

---
+++ 2026/04-07-palindrome-characters.js
@@ -0,0 +1,25 @@
+// Given a string, determine if it's a palindrome and return the middle character (if it's odd length) or middle two characters (if it's even).
+
+// A palindrome is a string that is the same forward and backward.
+// If it's not a palindrome, return "none".
+
+// Tests:
+// 1. palindromeLocator("racecar") should return "e".
+// 2. palindromeLocator("level") should return "v".
+// 3. palindromeLocator("freecodecamp") should return "none".
+// 4. palindromeLocator("noon") should return "oo".
+// 5. palindromeLocator("11100111") should return "00".
+
+function palindromeLocator(str) {
+  const reversed = str.split("").reverse().join("");
+
+  if (str !== reversed) return "none";
+
+  const middle = Math.floor(str.length / 2);
+
+  if (str.length % 2 === 0) {
+    return str.slice(middle - 1, middle + 1);
+  } else {
+    return str[middle];
+  }
+}
